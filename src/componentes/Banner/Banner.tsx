6b68461ac17dc834b6ed7e31c752b464ede3c4b9
MatheusDums <dumss@icloud.com>
2025-11-21 13:04:00 -0300

    Fim Aula 01

---
+++ src/componentes/Banner/Banner.tsx
@@ -0,0 +1,16 @@
+import './Banner.css'
+import React from 'react'
+
+interface BannerProps {
+    enderecoImagem: string
+    textoAlternativo?: string
+}
+
+export const Banner = ({enderecoImagem, textoAlternativo}: BannerProps) => {
+    // JSX
+    return (
+        <header className="banner">
+            <img src={enderecoImagem} alt={textoAlternativo}/>
+        </header>
+    )
+}
